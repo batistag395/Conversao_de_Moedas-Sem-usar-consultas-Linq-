fa59c3e8708b71e98f03620a0507c1cc5a7360cf
Gustavo Batista <batistag395@gmail.com>
2022-08-26 16:02:20 -0300

    primeiro commit do projketo conversor de moedas sem usar consultas linq

---
+++ ConversaoDeMoedas/Program.cs
@@ -0,0 +1,45 @@
+﻿using ConversaoDeMoedas;
+
+Moedascs moedas;
+
+List<Moedascs> MoedasList = new List<Moedascs>();
+
+string[] text = System.IO.File.ReadAllLines(@"C:\Users\gorde\Desktop\Moeda\moedas.txt");
+
+foreach (string line in text)
+{
+    string[] vect = line.Split(' ');
+    moedas = new Moedascs(vect[0], double.Parse(vect[1]));
+    MoedasList.Add(moedas);
+}
+
+Console.WriteLine("Imprimindo a lista");
+
+foreach (Moedascs m in MoedasList)
+{
+    Console.WriteLine(m);
+}
+
+Console.WriteLine("Qual a moeda que voce possui? ");
+string nomeMoedaEntrada = Console.ReadLine();
+
+Console.WriteLine("Qual valor que voce quer converter?");
+double valorMoedaOrigem = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Qual a moeda de saida para conversao?");
+string nomeMoedaDestino = Console.ReadLine();
+
+for (var i = 0; i < MoedasList.Count; i++)
+{
+    if (nomeMoedaEntrada == MoedasList[i].Currency)
+    {
+        for (var j = 0; j < MoedasList.Count; j++)
+        {
+            if (nomeMoedaDestino == MoedasList[j].Currency)
+            {
+                Console.WriteLine($"Valor total da conversão: {MoedasList[j].Currency} {((MoedasList[i].Rate / MoedasList[j].Rate) * valorMoedaOrigem).ToString("F2")}");
+            }
+
+        }
+    }
+}
